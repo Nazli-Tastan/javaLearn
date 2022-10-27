54b7ca473c00ee67a5fc85b43e8c8883737a6eb7
Nazlı <ahm.2012.123@gmail.com>
2022-10-27 18:33:25 +0300

    27 EKİM 2022

--- src/j22_DataTime/C01_LocalDate.java
+++ src/j22_DataTime/C01_LocalDate.java
@@ -1,9 +1,8 @@
 package j22_DataTime;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class CO1_LocalDate {
+public class C01_LocalDate {
     public static void main(String[] args) {
         LocalTime suAn=LocalTime.now();//Systemde anlık zamanı verir
         System.out.println("suAn = " + suAn);
